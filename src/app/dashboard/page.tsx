7f56dc3f80396bd8e628387076f5866b4eea0d8c
ecthon <ecthon@gmail.com>
2025-04-12 12:05:34 -0400

    refactor: update layout and improve link item structure in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,73 +1,71 @@
 import { AppSidebar } from "../../components/app-sidebar";
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { Link2, PenBox, Trash } from "lucide-react";
+import { ArrowRight, Link2 } from "lucide-react";
 
 export default function Page() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="flex flex-col aspect-video rounded-xl bg-muted/50 p-6">
-              <h1 className="font-bold text-zinc-800">Trabalho</h1>
-              <Separator orientation="horizontal" className="my-2" />
-              {Array.from({ length: 3 }).map((_, i) => (
-                <div
-                  key={i}
-                  className="group flex h-7 w-full items-center justify-center gap-2 rounded-md p-4 text-muted-foreground bg-transparent hover:bg-zinc-100 text-[13px] cursor-pointer"
-                >
-                  <Link2 className="h-4 w-4 group-hover:text-blue-600" />
-                  <p className=" text-zinc-800 font-medium">Next.js</p>
-                  <p className="text-zinc-500">https://nextjs.org/</p>
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbLink href="#">
+                                    Building Your Application
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </header>
+                <div className="flex flex-1 flex-col gap-4 p-4">
+                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                        <div className="flex flex-col aspect-video rounded-xl p-6">
+                            <h1 className="font-bold text-zinc-800">Trabalho</h1>
+                            <Separator orientation="horizontal" className="my-2" />
+                            {Array.from({ length: 3 }).map((_, i) => (
+                                <div
+                                    key={i}
+                                    className="group flex h-7 w-full items-center justify-center gap-2 p-4 text-muted-foreground bg-transparent hover:bg-gray-100 text-[13px] cursor-pointer"
+                                >
+                                    <Link2 className="h-4 w-4 group-hover:text-blue-600" />
+                                    <p className=" text-zinc-800 font-medium whitespace-nowrap">Nome do link aqui</p>
+                                    <p className="text-zinc-500 whitespace-nowrap overflow">https://nextjs.org/</p>
 
-                  <div className="ml-auto flex items-center gap-1">
-                    <button className="rounded-sm cursor-pointer p-1 hover:border-zinc-200 border border-transparent">
-                      <PenBox className="h-4 w-4 text-transparent group-hover:text-zinc-400 grou-hover:text-muted-foreground" />
-                    </button>
-                    <button className="rounded-sm cursor-pointer p-1 hover:border-zinc-200 border border-transparent">
-                      <Trash className="h-4 w-4 text-transparent group-hover:text-zinc-400 grou-hover:text-muted-foreground" />
-                    </button>
-                  </div>
+                                    <div className="ml-auto flex items-center gap-1">
+                                        <button className="flex justify-center items-center gap-2 rounded-sm cursor-pointer py-0.5 px-2 bg-transparent hover:bg-white">
+                                            <ArrowRight className="h-3.5 w-3.5" />
+                                            <span className="font-semibold">Abrir</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="aspect-video rounded-xl bg-muted/50" />
+                        <div className="aspect-video rounded-xl bg-muted/50" />
+                    </div>
+                    <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
                 </div>
-              ))}
-            </div>
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+            </SidebarInset>
+        </SidebarProvider>
+    );
 }
